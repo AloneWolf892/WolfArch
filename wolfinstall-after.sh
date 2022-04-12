129ce9f4c2ee0c08c9ed1824f7ec51668ed42b45
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2022-04-12 16:16:11 +0200

    Fixed typos

--- wolfinstall-after.sh
+++ wolfinstall-after.sh
@@ -1,5 +1,5 @@
-$USERNAME="akira"
-$PASSWORD="akira"
+LOCAL_USERNAME=akira
+LOCAL_PASSWORD=akira
 
 reflector -c Spain -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
@@ -24,17 +24,17 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 $HOSTNAME.localdomain   $HOSTNAME" >> /etc/hosts
 echo root:password | chpasswd
 
-useradd -m $USERNAME
-echo $USERNAME:$PASSWORD | chpasswd
+useradd -m $LOCAL_USERNAME
+echo $LOCAL_USERNAME:$PASSWORD | chpasswd
 
 
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
-sudo -u $USERNAME makepkg -si --noconfirm
+sudo -u $LOCAL_USERNAME makepkg -si --noconfirm
 cd ..
 
-sudo -u $USERNAME paru -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools linux-headers bluez bluez-utils pulseaudio-bluetooth cups openssh google-chrome chrome-gnome-shell zip wget curl qemu qemu-arch-extra virt-manager bridge-utils rsync --noconfirm
+sudo -u $LOCAL_USERNAME paru -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools linux-headers bluez bluez-utils pulseaudio-bluetooth cups openssh google-chrome chrome-gnome-shell zip wget curl qemu qemu-arch-extra virt-manager bridge-utils rsync --noconfirm
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCHLINUX
 grub-mkconfig -o /boot/grub/grub.cfg
