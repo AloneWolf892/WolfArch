1ef16cf557c20e93699ed6e723e490de57f563e3
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2022-04-12 09:46:24 +0200

    Fixed typo

--- wolfinstall.sh
+++ wolfinstall.sh
@@ -1,3 +1,3 @@
-CPU_THREADS = getconf _NPROCESSORS_ONLN
+CPU_THREADS=getconf _NPROCESSORS_ONLN
 sed -i "/^#ParallelDownloads/ c ParallelDownloads = $CPU_THREADS" /etc/pacman.conf
 sed -i "/^#Color/ c Color" /etc/pacman.conf
