30bc95a9e410bda70d5b98a6ce140c8e8db16de7
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2022-04-12 12:38:37 +0200

    Added partitioning utility

--- wolfinstall.zsh
+++ wolfinstall.zsh
@@ -6,3 +6,30 @@ pacman -Sy reflector --noconfirm
 reflector -c Spain -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy dialog neofetch --noconfirm
 neofetch
+
+PS3="Select the root partition: "
+COLUMNS=12
+select rootpartition in $(lsblk -n --output NAME -l)
+do
+        echo "$rootpartition will be used as the root partition"
+        mkfs.btrfs /dev/$rootpartition -f
+        mount /dev/$rootpartition /mnt
+        break
+done
+
+PS3="Select the boot partition: "
+select bootpartition in $(lsblk -n --output NAME -l)
+do
+        echo "$bootpartition will be used as the boot/efi partition"
+        mkdir /mnt/boot
+        mkdir /mnt/boot/efi
+        mount /dev/$bootpartition /mnt/boot/efi
+        break
+done
+
+PS3="Select the swap partition: "
+select swappartition in $(lsblk -n --output NAME -l)
+do
+        echo "$swappartition will be used as the swap partition"
+        break
+done
