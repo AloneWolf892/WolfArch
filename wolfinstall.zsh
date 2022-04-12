a33b6b93f7f861b709196fa4fba1f215f91d5e2f
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2022-04-12 11:38:15 +0200

    Update wolfinstall.zsh

--- wolfinstall.zsh
+++ wolfinstall.zsh
@@ -2,7 +2,7 @@ CPU_THREADS=$(getconf _NPROCESSORS_ONLN)
 sed -i "/^#ParallelDownloads/ c ParallelDownloads = $CPU_THREADS" /etc/pacman.conf
 sed -i "/^#Color/ c Color" /etc/pacman.conf
 timedatectl set-ntp true
-pacman -Sy reflector dialog neofetch --noconfirm
+pacman -Sy reflector --noconfirm
 reflector -c Spain -a 6 --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syy
+pacman -Sy dialog neofetch --noconfirm
 neofetch
