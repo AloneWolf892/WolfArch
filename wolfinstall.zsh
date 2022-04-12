87cf0d88b746a65fbb73ad3df65c2295317accdb
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2022-04-12 11:25:13 +0200

    Added reflector

--- wolfinstall.zsh
+++ wolfinstall.zsh
@@ -1,3 +1,7 @@
 CPU_THREADS=$(getconf _NPROCESSORS_ONLN)
 sed -i "/^#ParallelDownloads/ c ParallelDownloads = $CPU_THREADS" /etc/pacman.conf
 sed -i "/^#Color/ c Color" /etc/pacman.conf
+timedatectl set-ntp true
+pacman -Sy reflector dialog --noconfirm
+reflector -c Spain -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syyy
