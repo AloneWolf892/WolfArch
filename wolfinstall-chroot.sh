f8741c32bef96aa5af0b83b5032563c3bd93bb9d
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2022-04-20 13:32:34 +0200

    Added ani-cli

--- wolfinstall-chroot.sh
+++ wolfinstall-chroot.sh
@@ -132,7 +132,8 @@ ln -s $LOCAL_HOME/.zshrc /root/.zshrc
 ln -s $LOCAL_HOME/.p10k.zsh /root/.p10k.zsh
 ln -s $LOCAL_HOME/powerlevel10k /root/powerlevel10k
 
-
+paru -S openssl mpv aria2 ffmpeg --noconfirm
+paru -S ani-cli-git --noconfirm
 
 rm /etc/sudoers.d/$LOCAL_USERNAME
 echo "$LOCAL_USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/$LOCAL_USERNAME
